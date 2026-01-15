2e29c7072d5d520b28796c9a58779e444910620c
Kavindu Sandeepa Amaranath <kavindusandeepa20031030@gmail.com>
2026-01-15 15:21:54 +0530

    midify App.jsx, Register.jsx

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,35 @@
+function Register() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <form className="bg-white p-6 rounded-lg shadow-md w-80">
+                <h2 className="text-2xl font-bold mb-4 text-center">
+                    Register
+                </h2>
+
+                <input 
+                    type="text"
+                    placeholder="Name"
+                    className="w-full p-2 mb-3 border rounded"
+                />
+
+                <input 
+                    type="email"
+                    placeholder="Email"
+                    className="w-full p-2 mb-3 border rounded"
+                />
+
+                <input 
+                    type="password"
+                    placeholder="Password"
+                    className="w-full p-2 mb-4 border rounded"
+                />
+
+                <button className="w-full bg-green-600 text-white py-2 rounded hover:bg-green-700">
+                    Register
+                </button>
+            </form>
+        </div>
+    );
+}
+
+export default Register;
